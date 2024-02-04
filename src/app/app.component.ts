87b42659b55ed4569e5dde4b4ad1743f9c660b21
Kenny Lai <kennylai360@gmail.com>
2024-02-04 12:25:44 +0000

    Updated favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,9 @@ export class AppComponent {
     'Wouldnt you reconsider?',
     'Is that your final answer?',
     'Pretty pweasssee?',
+    "Fine, I'll stop asking...",
+    'Just kidding! PLEASE SAY YES!',
+    "Okay, I'm really really sad now :'(",
     "You're breaking my kokoro ;(",
   ];
 
